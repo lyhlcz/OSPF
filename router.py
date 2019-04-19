7947b9dcf6e128a8216f32abc74b27ef13a9706e
lyh <2256551717@qq.com>
2019-04-19 16:33:25 +0800

    Create router.py

---
+++ router.py
@@ -0,0 +1,23 @@
+import multiprocessing as mp
+
+
+class ROUTER:
+    def __init__(self, i, neighbors):
+        self.ID = i
+        self.neighbors = neighbors
+        self.table = []
+
+    # 交换路由表
+    def exchange(self):
+        return
+
+    # 输出路由表
+    def getTable(self):
+        print(self.table)
+
+
+def router_process():
+    return
+
+
+
